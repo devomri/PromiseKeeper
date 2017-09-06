c919d52d54a10f9b8efe56c7960b153b454c5492
Omri A <devomri@gmail.com>
2017-09-06 21:35:09 +0300

    about activity updated

--- app/src/main/java/com/omri/dev/promisekeeper/AboutActivity.java
+++ app/src/main/java/com/omri/dev/promisekeeper/AboutActivity.java
@@ -4,17 +4,20 @@ import android.database.Cursor;
 import android.provider.CallLog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.TextView;
 
 import java.util.Date;
 
 public class AboutActivity extends AppCompatActivity {
-
     @Override
-    @SuppressWarnings({"MissingPermission"})
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    public void closeAboutActivity(View view) {
+        finish();
     }
 }
